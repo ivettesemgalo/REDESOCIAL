a54d793ef02f57cd778187a63ff466e8b6bb9945
ivettesemgalo <105220815+ivettesemgalo@users.noreply.github.com>
2024-11-08 10:46:00 -0300

    sdads

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,8 +1,16 @@
 const url='https://github.com/guilhermeonrails/api'
 
-async function visualizar informacoesGlobais(){
+async function visualizarInformacoesGlobais(){
 const res= await fetch(url)
-const dados =  await res.json()
-console.log(dados);
+const dados = await res.json()
+const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos-conteiner_texto')
+paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo'} de pessoas e
+que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social
+e passam em média ${dados.tempo_medio} horas conectadas.`
+console.log(paragrafo)
+
+const conteiner = document.getElementById('graficos-container)
+container.aapendChild(paragrafo)
 }
 visualizarinformacoesGlobais()
